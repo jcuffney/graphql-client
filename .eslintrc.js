33c4dc6ea2c558cd976b53940b2091fcf14ffcb0
Joseph Cuffney <josephcuffney@gmail.com>
2020-12-11 17:41:07 -0500

    lint

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+
+module.exports = {
+    "extends": "airbnb",
+    "rules": {
+        'react/jsx-filename': [0],
+        'react/jsx-filename-extension': [0],
+        'import/no-unresolved': [0],
+    }
+};
--- src/views/App/index.js
+++ src/views/App/index.js
@@ -1,17 +1,21 @@
 import React, { memo } from 'react';
+import { applyTo, pipe } from 'ramda';
 import logo from './logo.svg';
 import styles from './App.module.scss';
-import { applyTo, pipe } from 'ramda';
 
-export default applyTo(({}) => (
-  <div className={ styles.App }>
-    <header className={ styles.AppHeader }>
-      <img src={logo} className={ styles.AppLogo } alt="logo" />
+export default applyTo(() => (
+  <div className={styles.App}>
+    <header className={styles.AppHeader}>
+      <img src={logo} className={styles.AppLogo} alt="logo" />
       <p>
-        Edit <code>src/App.js</code> and save to reload.
+        Edit
+        {' '}
+        <code>src/App.js</code>
+        {' '}
+        and save to reload.
       </p>
       <a
-        className={ styles.AppLink }
+        className={styles.AppLink}
         href="https://reactjs.org"
         target="_blank"
         rel="noopener noreferrer"
