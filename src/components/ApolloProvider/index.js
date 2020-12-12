d5189ed961ae14c2ae6a17e8155c94310951230f
Joseph Cuffney <josephcuffney@gmail.com>
2020-12-11 19:31:42 -0500

    use auth header

--- src/components/ApolloProvider/index.js
+++ src/components/ApolloProvider/index.js
@@ -1,9 +1,48 @@
 import React, { memo } from 'react';
 import { applyTo, pipe } from 'ramda';
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import fetch from 'unfetch';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  ApolloLink,
+  HttpLink,
+} from '@apollo/client';
+import { onError } from 'apollo-link-error';
+// import { useAuth0 } from "@auth0/auth0-react";
+
+const httpLink = new HttpLink({
+  uri: 'https://api.cuffney.com/v1/graphql',
+  fetch,
+});
+
+const authMiddleware = new ApolloLink(async (operation, forward) => {
+  // const { getAccessTokenSilently } = useAuth0();
+  // const accessToken = await getAccessTokenSilently();
+  operation.setContext({
+    headers: {
+      // authorization: `Bearer ${ accessToken }`,
+      authorization: 'allow',
+    },
+  });
+  return forward(operation);
+});
 
 const client = new ApolloClient({
-  uri: 'https://api.cuffney.com/v1/graphql',
+  link: ApolloLink.from([
+    authMiddleware,
+    onError(({ graphQLErrors, networkError }) => {
+      if (graphQLErrors) {
+        graphQLErrors.forEach(({ message, locations, path }) => {
+          // eslint-disable-next-line
+          console.error(`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`);
+        });
+      }
+      // eslint-disable-next-line
+      if (networkError) console.log(`[Network error]: ${networkError}`);
+    }),
+    httpLink,
+  ]),
   cache: new InMemoryCache(),
 });
 
