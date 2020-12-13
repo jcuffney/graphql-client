cdf79cb32b83357e4d5db386e8d919bd4271f7dd
Joseph Cuffney <josephcuffney@gmail.com>
2020-12-13 15:36:30 -0500

    rn thow

--- src/index.js
+++ src/index.js
@@ -49,5 +49,3 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-throw new Error('sentry testing');
