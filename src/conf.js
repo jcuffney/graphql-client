56db5ef564463a63793f7ec3d4cbf8a4b797c48f
Joseph Cuffney <josephcuffney@gmail.com>
2020-12-13 15:36:08 -0500

    sentry

--- src/conf.js
+++ src/conf.js
@@ -2,6 +2,7 @@ const {
   REACT_APP_NODE_ENV: NODE_ENV,
   REACT_APP_API_HOST: API_HOST,
   REACT_APP_STRIPE_PUBLISHABLE_KEY: STRIPE_PUBLISHABLE_KEY,
+  REACT_APP_SENTRY_DSN: SENTRY_DSN,
 } = process.env;
 
 export const stripe = window.Stripe(STRIPE_PUBLISHABLE_KEY); // eslint-disable-line
@@ -10,4 +11,5 @@ export default {
   stripe,
   API_HOST,
   NODE_ENV,
+  SENTRY_DSN,
 };
--- src/index.js
+++ src/index.js
@@ -3,15 +3,30 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { StylesProvider } from '@material-ui/core/styles';
 import { MuiThemeProvider, CssBaseline } from '@material-ui/core';
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
 
 import App from './views/App';
 import reportWebVitals from './reportWebVitals'; // eslint-disable-line
 import ApolloProvider from './components/ApolloProvider';
 import theme from './theme';
 import GlobalStyles from './GlobalStyles';
+import config from './conf';
 
 import './index.scss';
 import 'fontsource-roboto';
+
+Sentry.init({
+  dsn: config.SENTRY_DSN,
+  autoSessionTracking: true,
+  integrations: [
+    new Integrations.BrowserTracing(),
+  ],
+
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
   <React.StrictMode>
@@ -34,3 +49,5 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+throw new Error('sentry testing');
